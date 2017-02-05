ea118b058fa3870aea24530c6aa7282b45e12c05
doewie <douwe@laranja.nl>
2017-02-05 13:20:24 +0000

    add testfunctions for serial API

---
+++ serial-API.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"go.bug.st/serial.v1"
+	"go.bug.st/serial.v1/enumerator"
+)
+
+// print serial ports
+
+func printPorts() {
+
+	ports, err := serial.GetPortsList()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(ports) == 0 {
+		log.Fatal("No serial ports found!")
+	}
+	for _, port := range ports {
+		fmt.Printf("Found port: %v\n", port)
+	}
+}
+
+func printPortDetails() {
+	ports, err := enumerator.GetDetailedPortsList()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(ports) == 0 {
+		fmt.Println("No serial ports found!")
+		return
+	}
+	for _, port := range ports {
+		fmt.Printf("Found port: %s\n", port.Name)
+		if port.IsUSB {
+			fmt.Printf("   USB ID     %s:%s\n", port.VID, port.PID)
+			fmt.Printf("   USB serial %s\n", port.SerialNumber)
+		}
+	}
+}
