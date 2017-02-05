150a0af89a5ab6742b2b0df27eb98fd2ee945d51
doewie <douwe@laranja.nl>
2017-02-05 22:11:33 +0000

    add some basic functions

--- main.go
+++ main.go
@@ -1,6 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
+var (
+	serialRecv = make(chan []byte)
+	serialSend = make(chan []byte)
+	done       = make(chan error)
+)
+
 func main() {
-	printPorts()
-	printPortDetails()
+	// printPorts()
+	// printPortDetails()
+	openPortCom5()
+
+	go SerialDispatch()
+
+	if err, ok := <-done; ok {
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		os.Exit(1)
+	}
+}
+
+func check(e error) {
+	if e != nil {
+		done <- e
+	}
 }
